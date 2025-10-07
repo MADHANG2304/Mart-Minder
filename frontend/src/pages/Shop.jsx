b5bb27ed551ba1966d632a266058fecc0e0583d0
MADHANG2304 <madhang2306@gmail.com>
2025-10-07 09:55:04 +0530

    Shop categories were designed.

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -7,29 +7,148 @@ import img4 from '../assets/ShopHero/img4.jpg'
 
 const Shop = () => {
   return (
-    <div className=''>
-        <Navbar />
+    <>
+    <div className='bg-custWhite'>
+        <div className=''>
+            <Navbar />
+            <div
+                className="hero h-72"
+                style={{
+                    backgroundImage:
+                    `url(${img2})`,
+                }}
+                >
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-neutral-content ">
+                    <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Find Products</h1>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Search items</legend>
+                        <input type="text" className="input" placeholder="Search" />
+                        <p className="label">You can search and get the list of items here.</p>
+                    </fieldset>
+                    </div>
+                </div>
+            </div> 
+        </div>
 
-        <div
-            className="hero h-96"
-            style={{
-                backgroundImage:
-                `url(${img2})`,
-            }}
-            >
-            <div className="hero-overlay"></div>
-            <div className="hero-content text-neutral-content ">
-                <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend">Search items..</legend>
-                    <input type="text" className="input" placeholder="Search" />
-                    <p className="label">You can search and get the list of items here.</p>
-                </fieldset>
+        <div className='flex flex-wrap gap-5 justify-center p-5'>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img1}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
                 </div>
             </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img2}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img3}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img4}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img3}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img1}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img4}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card  image-full w-96 shadow-sm">
+                <figure>
+                    <img
+                    src={img2}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn bg-secondaryGreen text-custWhite hover:bg-custWhite hover:text-secondaryGreen">View More</button>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     </div>
+    </>
   )
 }
 
