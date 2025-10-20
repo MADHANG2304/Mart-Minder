b80f32b46a34baf50b7b32e576bce16b5b978a86
MADHANG2304 <madhang2306@gmail.com>
2025-10-20 18:27:37 +0530

    Market.js is implemented successfully

--- backend/routes/market.js
+++ backend/routes/market.js
@@ -26,4 +26,53 @@ router.get("/get-markets" , async (req , res) => {
     }
 })
 
+router.delete("/:id" , async (req , res) => {
+    try {
+        const deleteMarket = await Market.findByIdAndDelete(req.params.id);
+        if(!deleteMarket)
+        {
+            return res.status(404).json({ error: 'Market not found' });
+        }
+        return res.status(200).json({ message: 'Market deleted successfully' });
+    } catch (error) {
+        return res.status(500).json({error : "Error deleting Market"});
+    }
+})
+
+router.get("/:id" , async (req , res) => {
+    try {
+        const market = await Market.findById(req.params.id);
+        if(!market)
+        {
+            return res.status(404).json({ error: 'Market not found' });
+        }
+        return res.status(200).json({ market });
+    } catch (error) {
+        return res.status(500).json({error : "Error fetching Market"});
+    }   
+})
+
+router.put("/:id" , async (req , res) => {
+    const {name , description , location , activeHours , imageUrl} = req.body;
+    if(!name || !description || !location || !activeHours || !imageUrl)
+    {
+        return res.status(400).json({error : "Please fill all the fields"});
+    }
+    try {
+        const updatedMarket = await
+            Market.findByIdAndUpdate(
+                req.params.id,
+                {name , description , location , activeHours , imageUrl},
+                {new: true}
+            );
+        if(!updatedMarket)
+        {
+            return res.status(404).json({ error: 'Market not found' });
+        }
+        return res.status(200).json({ message: 'Market updated successfully', market: updatedMarket });
+    } catch (error) {
+        return res.status(500).json({error : "Error updating Market"});
+    }
+})
+
 module.exports = router;
